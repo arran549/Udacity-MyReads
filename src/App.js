6c27d68d6856ed296d3fdbc9921ac8a605915981
Arran Dyer <arran549@gmail.com>
2018-01-28 16:36:55 +0000

    feat: get books from server and set to App state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Book from './Book.js'
 
 class BooksApp extends React.Component {
   state = {
+    books: [],
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -13,6 +14,13 @@ class BooksApp extends React.Component {
      */
     showSearchPage: false
   }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
+
 
   render() {
     return (
