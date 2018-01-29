6cfac61d776169decb9ec02448abf0a75c1cf564
Arran Dyer <arran549@gmail.com>
2018-01-29 19:29:58 +0000

    refactor: remove console.log

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -10,8 +10,6 @@ class Bookshelf extends Component {
     }
 
     render() {
-        console.log(this.props.books)
-
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{this.props.title}</h2>
