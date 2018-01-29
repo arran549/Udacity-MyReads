c1333b4a9f11ff901822bf597d60ac11fe889278
Arran Dyer <arran549@gmail.com>
2018-01-29 18:48:08 +0000

    refactor: rename props title

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -4,15 +4,15 @@ import { PropTypes } from 'prop-types'
 class BookDetails extends Component {
 
     static propTypes = {
-        bookTitle: PropTypes.string.isRequired,
         bookAuthors: PropTypes.string.isRequired
+        title: PropTypes.string.isRequired,
     }
 
     render() {
         return (
             <div>
-                <div className="book-title">{this.props.bookTitle}</div>
                 <div className="book-authors">{this.props.bookAuthors}</div>
+                <div className="book-title">{this.props.title}</div>
             </div>
         )
     }
