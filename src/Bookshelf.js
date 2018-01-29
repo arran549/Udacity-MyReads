026967935b1b48ccd1024afa72c388c7dbb1aeb9
Arran Dyer <arran549@gmail.com>
2018-01-29 19:43:25 +0000

    refactor:  use this.props alias

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -10,12 +10,15 @@ class Bookshelf extends Component {
     }
 
     render() {
+
+        const { title, books } = this.props
+
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <h2 className="bookshelf-title">{title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                        {this.props.books.map((book) => (
+                        {books.map((book) => (
                             <li key={book.id}><Book book={book}/></li>
                         ))}
                     </ol>
