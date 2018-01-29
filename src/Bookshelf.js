554742492e733d0be5d59d74016a1109d419b8c0
Arran Dyer <arran549@gmail.com>
2018-01-29 18:46:34 +0000

    refactor: add Bookshelf component

---
+++ src/Bookshelf.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Book from './Book'
+
+class Bookshelf extends Component {
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        books: PropTypes.array.isRequired
+    }
+
+    render() {
+        console.log(this.props.books)
+
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {this.props.books.map((book) => (
+                            <li key={book.id}><Book book={book}/></li>
+                        ))}
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Bookshelf
